4822ced9157de4aca844d9f0523f2dbc599fb639
prabish01 <prabishdangi2002@gmail.com>
2025-12-19 11:12:09 +0545

    added menue

---
+++ src/class-menus/class-2-menu/script.js
@@ -0,0 +1,16 @@
+// Class 2 Menu Navigation
+document.getElementById("flip").addEventListener("click", () => {
+  window.location.href = "../../class-book-selections/class-2/index.html";
+});
+
+document.getElementById("anima").addEventListener("click", () => {
+  window.location.href = "../../class-animations/animation-selection/index.html";
+});
+
+document.getElementById("test").addEventListener("click", () => {
+  window.location.href = "../../class-tests/test-selection/index.html";
+});
+
+document.getElementById("worksheet").addEventListener("click", () => {
+  window.location.href = "../../class-worksheets/worksheet-selection/index.html";
+});
---
+++ src/class-menus/class-5-menu/script.js
@@ -0,0 +1,16 @@
+// Class 5 Menu Navigation
+document.getElementById("flip").addEventListener("click", () => {
+  window.location.href = "../../class-book-selections/class-5/index.html";
+});
+
+document.getElementById("anima").addEventListener("click", () => {
+  window.location.href = "../../class-animations/animation-selection/index.html";
+});
+
+document.getElementById("test").addEventListener("click", () => {
+  window.location.href = "../../class-tests/test-selection/index.html";
+});
+
+document.getElementById("worksheet").addEventListener("click", () => {
+  window.location.href = "../../class-worksheets/worksheet-selection/index.html";
+});
