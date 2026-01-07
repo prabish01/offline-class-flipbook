3929027502a5cf6be403416badcaee7d183deb2d
prabish01 <prabishdangi2002@gmail.com>
2026-01-07 23:50:20 +0545

    c6 and c8 flipbook done

--- src/class-menus/class-6-menu/script.js
+++ src/class-menus/class-6-menu/script.js
@@ -1,4 +1,8 @@
 // Class 6 Menu Navigation
+document.getElementById("flip").addEventListener("click", () => {
+  window.location.href = "../../class-flipbooks/class-6/index.html";
+});
+
 document.getElementById("test").addEventListener("click", () => {
   window.location.href = "../../class-tests/test-generator/class-6.html";
 });
---
+++ src/class-menus/class-8-menu/script.js
@@ -0,0 +1,4 @@
+// Class 8 Menu Navigation
+document.getElementById("flip").addEventListener("click", () => {
+  window.location.href = "../../class-flipbooks/class-8/index.html";
+});
