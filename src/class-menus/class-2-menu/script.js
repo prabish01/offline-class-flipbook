31b480273a630d0fcb7bc7fd527f44c6b62785d2
prabish01 <prabishdangi2002@gmail.com>
2026-03-04 23:33:52 +0545

    added class 8 videos and updated chapter wise workbook

--- src/class-menus/class-2-menu/script.js
+++ src/class-menus/class-2-menu/script.js
@@ -4,7 +4,7 @@ document.getElementById("flip").addEventListener("click", () => {
 });
 
 document.getElementById("anima").addEventListener("click", () => {
-  alert("Class 2 animations coming soon!");
+  window.location.href = "../../class-animations/class-2-animations/index.html";
 });
 
 document.getElementById("test").addEventListener("click", () => {
--- src/class-menus/class-8-menu/script.js
+++ src/class-menus/class-8-menu/script.js
@@ -2,3 +2,7 @@
 document.getElementById("flip").addEventListener("click", () => {
   window.location.href = "../../class-flipbooks/class-8/index.html";
 });
+
+document.getElementById("anima").addEventListener("click", () => {
+  window.location.href = "../../class-animations/class-8-animations/index.html";
+});
